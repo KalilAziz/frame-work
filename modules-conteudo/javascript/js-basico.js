ba6cc61350c0d7d17734380428fc70155c4eb4f6
KalilAziz <kalilazizsantoschami@hotmail.com>
2022-01-05 17:05:02 -0300

    configuração do conteudo js-basico 11 a 17

--- modules-conteudo/javascript/js-basico.js
+++ modules-conteudo/javascript/js-basico.js
@@ -9,14 +9,16 @@ sectionBasico.innerHTML = `
     <ul>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/01 - console.html">Console</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/02 - comentario.html">Comentários no código</a></li>
+                    <li><a class="inativo" href="">Typeof</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/03 - variavel com let.html">Variável com Let</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/04 - variavel-com-const.html">Variável com const</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/05 - let-vs-var.html">Let vs Var</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/06 - tipos-de-dados-primitivos.html">Tipos de dados primitivos</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/07 - operadores-aritimeticos.html">Operadores aritméticos</a></li>
-                    <li><a class="basico" href="../../conteudo/javascript/iniciante/08 - atrinuicoes.html">Atribuições</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/09 - incremento.html">Incremento e Decremento</a></li>
+                    <li><a class="basico" href="../../conteudo/javascript/iniciante/08 - atrinuicoes.html">Atribuições</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/10 - alert-confirm-e-promt.html">Alert, confirm e promt</a></li>
+                    <li><a class="inativo" href="">.length</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/11 - string.html">Mais sobre string</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/12 - numbers.html">Mais sobre Numbers</a></li>
                     <li><a class="basico" href="../../conteudo/javascript/iniciante/13 - objeto-math.html">Objeto Math</a></li>
